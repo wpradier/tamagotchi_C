3ba0ee25a3566a10acdbb5dabd0c3e0832243043
marina robert <marina.robert1508@gmail.com>
2022-01-09 19:52:40 +0100

    test compile with window glade

--- test-compile.c
+++ test-compile.c
@@ -1,28 +1,19 @@
-#include <gtk/gtk.h>
+ #include <gtk/gtk.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
-{
-  GtkWidget *window;
+    int
+    main(int argc, char *argv [])
+    {
+      GtkBuilder *gtkBuilder; 
+      GtkWidget *window; 
+      gtk_init(&argc, &argv); 
+      
+      gtkBuilder = gtk_builder_new(); 
+      gtk_builder_add_from_file(gtkBuilder, "test-glade.glade", NULL); 
+      window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "main_window")); 
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
-}
+     g_object_unref(G_OBJECT(gtkBuilder)); 
+     gtk_widget_show_all(window);  
+      gtk_main();
 
-int
-main (int    argc,
-      char **argv)
-{
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
-}
+      return 0;
+    }
