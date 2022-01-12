11bd8f6b1ff0b224a4e64a274966dfc9b3646c37
marina robert <marina.robert1508@gmail.com>
2022-01-12 22:26:09 +0100

    change command make and create hello word

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <gtk/gtk.h>
 
-int		main(int ac, char **av) {
-	printf("Hello World !");
+int	main(int argc, char **argv) {
+    //init variable
+	GtkBuilder *gtkBuilder;
+    GtkWidget *window;
 
-	return EXIT_SUCCESS;
+    gtk_init(&argc, &argv);
+
+    gtkBuilder = gtk_builder_new();
+    gtk_builder_add_from_file(gtkBuilder, "test-glade.glade", NULL);
+    window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "main_window"));
+
+    g_object_unref(G_OBJECT(gtkBuilder));
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return EXIT_SUCCESS;
 }
