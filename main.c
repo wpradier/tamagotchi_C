1b89234f21fc0329486144133833df7411d3730d
marina robert <marina.robert1508@gmail.com>
2022-01-16 23:20:14 +0100

    get and set text label

--- main.c
+++ main.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <gtk/gtk.h>
+ #include <gtk/gtk.h>
+ 
+int     main(int argc, char **argv) {
+//init variable
+GtkBuilder *gtkBuilder;
+GtkWidget *window;
+GtkWidget *label_test;
 
-int	main(int argc, char **argv) {
-    //init variable
-	GtkBuilder *gtkBuilder;
-    GtkWidget *window;
+gtk_init(&argc, &argv);
 
-    gtk_init(&argc, &argv);
-
-    gtkBuilder = gtk_builder_new();
-    gtk_builder_add_from_file(gtkBuilder, "test-glade.glade", NULL);
+gtkBuilder = gtk_builder_new();
+     gtkBuilder = gtk_builder_new_from_file("test-glade.glade");
     window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "main_window"));
+    gtk_builder_connect_signals(gtkBuilder, NULL);
 
-    g_object_unref(G_OBJECT(gtkBuilder));
-    gtk_widget_show_all(window);
-    gtk_main();
+    char *lol = "test";
+    label_test = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "label_test"));
 
-    return EXIT_SUCCESS;
+gchar *recep = gtk_label_get_text(GTK_LABEL(label_test));
+g_print("%s", recep);
+  
+  gtk_label_set_text(GTK_LABEL(label_test), lol);
+ 
+   gtk_widget_show_all(window);
+
+ gtk_main();
+
+return EXIT_SUCCESS;
 }
