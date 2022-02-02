6589ecbb63d04bf3fb2f861bee7dfbb1c077b09c
marina robert <marina.robert1508@gmail.com>
2022-02-03 00:02:09 +0100

    progress bar and button configured

--- srcs/game_graphic.c
+++ srcs/game_graphic.c
@@ -1,7 +1,9 @@
 #include "tamagotchi.h"
 
 static void buttonClicked();
-void updateFood();
+void updateFood(GtkWidget *widget, gpointer data);
+void updateHealth(GtkWidget *widget, gpointer data);
+void updateHygiene(GtkWidget *widget, gpointer data);
 void gamePlay();
 
 static void buttonClicked(){
@@ -10,7 +12,57 @@ static void buttonClicked(){
 
 void updateFood(GtkWidget *widget, gpointer data){
     if (widget) g_print("Modification progresse barre\n");
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(data), 0.9);
+    double value = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(data));
+    value -= 0.25;
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(data), value);
+
+    /* time */
+    /* read time */
+     time_t now = time (NULL);
+
+     /* convert in localtime */
+     struct tm tm_now = *localtime (&now);
+
+     /* create char JJ/MM/AAAA HH:MM:SS */
+     char s_now[sizeof "JJ/MM/AAAA HH:MM:SS"];
+
+     strftime (s_now, sizeof s_now, "%d/%m/%Y %H:%M:%S", &tm_now);
+
+     /* print result : */
+     g_print("'%s'\n", s_now);
+
+     // pour comparer deux dates : difftime()
+}
+
+void updateHealth(GtkWidget *widget, gpointer data){
+    if (widget) g_print("Modification progresse barre\n");
+    double value = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(data));
+    value = 1;
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(data), value);
+
+    /* time */
+    /* read time */
+     time_t now = time (NULL);
+
+     /* convert in localtime */
+     struct tm tm_now = *localtime (&now);
+
+     /* create char JJ/MM/AAAA HH:MM:SS */
+     char s_now[sizeof "JJ/MM/AAAA HH:MM:SS"];
+
+     strftime (s_now, sizeof s_now, "%d/%m/%Y %H:%M:%S", &tm_now);
+
+     /* print result : */
+     g_print("'%s'\n", s_now);
+
+     // pour comparer deux dates : difftime()
+}
+
+void updateHygiene(GtkWidget *widget, gpointer data){
+    if (widget) g_print("Modification progresse barre\n");
+    double value = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(data));
+    value += 0.25;
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(data), value);
 
     /* time */
     /* read time */
@@ -71,6 +123,11 @@ void game_graphic(int *argc, char***argv)
   GtkWidget *edit_name_button;
   GtkWidget *name_input;
   GtkWidget *game_button;
+  GtkWidget *health_bar;
+  GtkWidget *health_button;
+  GtkWidget *hygiene_bar;
+  GtkWidget *hygiene_button;
+
   //GtkWidget *name_label;
 
 	/* init gtk */
@@ -98,6 +155,20 @@ void game_graphic(int *argc, char***argv)
   /*init button food */
   food_button = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "button_food"));
   g_signal_connect(food_button, "clicked", G_CALLBACK(updateFood), (gpointer) food_bar);
+
+  /* init container health_bar */
+	health_bar = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "health_bar"));
+
+  /*init button health */
+  health_button = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "health_button"));
+  g_signal_connect(health_button, "clicked", G_CALLBACK(updateHealth), (gpointer) health_bar);
+
+  /* init container hygiene_bar */
+	hygiene_bar = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "hygiene_bar"));
+
+  /*init button hygiene */
+  hygiene_button = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "hygiene_button"));
+  g_signal_connect(hygiene_button, "clicked", G_CALLBACK(updateHygiene), (gpointer) hygiene_bar);
 
   /* init button game */
   game_button = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "game_button"));
